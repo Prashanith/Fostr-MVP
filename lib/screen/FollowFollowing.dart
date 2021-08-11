e5a088f9e7309b2abba80c31c63daaeb7d2403b0
tanharpatel <tanhacodes@gmail.com>
2021-08-12 01:29:38 +0530

    logo, follow following, refresh dashboard

--- lib/screen/FollowFollowing.dart
+++ lib/screen/FollowFollowing.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fostr/core/constants.dart';
 import 'package:fostr/models/UserModel/User.dart';
 import 'package:fostr/providers/AuthProvider.dart';
+import 'package:fostr/screen/ExternalUserProfile.dart';
 import 'package:fostr/services/UserService.dart';
 import 'package:fostr/utils/theme.dart';
 import 'package:fostr/widgets/Layout.dart';
@@ -130,115 +132,127 @@ class _UserCardState extends State<UserCard> with FostrTheme {
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthProvider>(context);
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 20),
-      height: 65,
-      width: 80.w,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(29),
-        color: Color(0xffEBFFEE),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 4),
-            blurRadius: 16,
-            color: Colors.black.withOpacity(0.25),
-          )
-        ],
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        CupertinoPageRoute(
+          builder: (context) {
+            return ExternalProfilePage(
+              user: user,
+            );
+          },
+        ),
       ),
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: 45,
-            width: 45,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: (user.userProfile != null)
-                      ? (user.userProfile?.profileImage != null)
-                          ? Image.network(
-                              user.userProfile!.profileImage!,
-                              height: 30,
-                              width: 25,
-                            ).image
-                          : Image.asset(IMAGES + "profile.png").image
-                      : Image.asset(IMAGES + "profile.png").image),
-            ),
-          ),
-          SizedBox(
-            width: 20,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                user.name,
-                style:
-                    h1.copyWith(fontSize: 14.sp, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "@" + user.userName,
-                style: h2.copyWith(fontSize: 14),
-              )
-            ],
-          ),
-          Spacer(),
-          (!widget.isFollower)
-            ? InkWell(
-              onTap: () async {
-                try {
-                  if (!widget.isFollower) {
-                    if (!followed) {
-                      var newUser =
-                          await userService.followUser(auth.user!, user);
-                      setState(() {
-                        followed = true;
-                      });
-                      auth.refreshUser(newUser);
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(followedSnackBar);
-                    } else {
-                      var newUser =
-                          await userService.unfollowUser(auth.user!, user);
-                      setState(() {
-                        followed = false;
-                      });
-                      auth.refreshUser(newUser);
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(unfollowedSnackBar);
-                    }
-                  }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.all(10),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: h2.color,
-                ),
-                child: Text(
-                  (widget.isFollower)
-                      ? ""
-                      : (followed)
-                          ? "Unfollow"
-                          : "Follow",
-                  style: h2.copyWith(color: Colors.white),
-                ),
-              ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: 65,
+        width: 80.w,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(29),
+          color: Color(0xffEBFFEE),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 4),
+              blurRadius: 16,
+              color: Colors.black.withOpacity(0.25),
             )
-            : Container()
-        ],
+          ],
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: (user.userProfile != null)
+                        ? (user.userProfile?.profileImage != null)
+                            ? Image.network(
+                                user.userProfile!.profileImage!,
+                                height: 30,
+                                width: 25,
+                              ).image
+                            : Image.asset(IMAGES + "profile.png").image
+                        : Image.asset(IMAGES + "profile.png").image),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  user.name,
+                  style:
+                      h1.copyWith(fontSize: 14.sp, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "@" + user.userName,
+                  style: h2.copyWith(fontSize: 14),
+                )
+              ],
+            ),
+            Spacer(),
+            (!widget.isFollower)
+              ? InkWell(
+                onTap: () async {
+                  try {
+                    if (!widget.isFollower) {
+                      if (!followed) {
+                        var newUser =
+                            await userService.followUser(auth.user!, user);
+                        setState(() {
+                          followed = true;
+                        });
+                        auth.refreshUser(newUser);
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(followedSnackBar);
+                      } else {
+                        var newUser =
+                            await userService.unfollowUser(auth.user!, user);
+                        setState(() {
+                          followed = false;
+                        });
+                        auth.refreshUser(newUser);
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(unfollowedSnackBar);
+                      }
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: h2.color,
+                  ),
+                  child: Text(
+                    (widget.isFollower)
+                        ? ""
+                        : (followed)
+                            ? "Unfollow"
+                            : "Follow",
+                    style: h2.copyWith(color: Colors.white),
+                  ),
+                ),
+              )
+              : Container()
+          ],
+        ),
       ),
     );
   }
