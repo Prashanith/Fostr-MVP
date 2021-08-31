8c4c216d8b19b04b1c9dc1db340aae3385ff4ab0
Miten Gajjar <miten3377@gmail.com>
2021-08-31 20:38:05 +0530

    in progress ongoing room bugs

--- android/app/src/main/kotlin/com/clubfostr/fostr/MainActivity.kt
+++ android/app/src/main/kotlin/com/clubfostr/fostr/MainActivity.kt
@@ -1,6 +1,43 @@
 package com.clubfostr.fostr
 
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import okhttp3.*
+import java.io.IOException
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+import kotlin.math.log
 
 class MainActivity: FlutterActivity() {
+//    private val client = OkHttpClient()
+//    private fun clean(){
+////    URL("https://us-central1-fostr2021.cloudfunctions.net/updateParticipants").openConnection() as HttpsURLConnection
+//    }
+//
+//    fun run(url: String) {
+//        val request = Request.Builder()
+//            .url(url)
+//            .build()
+//        Log.d("run","inner")
+//        client.newCall(request).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {}
+//            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+//        })
+//        Log.d("run","inner-lower")
+//    }
+//
+////    override fun onStart() {
+////        super.onStart()
+////        Log.d("onStart","calling")
+////        run("https://us-central1-fostr2021.cloudfunctions.net/updateParticipants")
+//////        clean()
+////    }
+////
+////    override fun onDestroy() {
+////        Log.d("onDestroy","upper")
+////        run("https://us-central1-fostr2021.cloudfunctions.net/updateParticipants")
+////        super.onDestroy()
+////        Log.d("onDestroy","lower")
+////
+////    }
 }
